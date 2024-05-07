c05a2291386932ba45902782cf6f7455567362ee
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-07 11:01:33 +0200

    Update display_page.py

--- src/display_page.py
+++ src/display_page.py
@@ -13,7 +13,20 @@ def load_image(image_path, band_idxs=range(12)):
     img_arr = tiff.imread(image_path)[:, :, band_idxs]
     print(f'image {image_path} loaded')
     return img_arr
-
+def preprocess_image(image_arr, num_bands=12):
+     # Image preprocessing
+    image_arr = image_arr.astype('float32')
+    min_vals = tf.reduce_min(image_arr, axis=(0, 1), keepdims=True) # image_arr shape is (256, 256, num_bands)
+    max_vals = tf.reduce_max(image_arr, axis=(0, 1), keepdims=True)
+    image_arr_normalized = tf.divide(tf.subtract(image_arr, min_vals), tf.subtract(max_vals, min_vals)+tf.constant(1e-3, dtype=tf.float32))
+    # Setting shapes of images & masks
+    image_arr_normalized.set_shape([256, 256, num_bands])
+    image_arr_normalized = np.expand_dims(image_arr_normalized, axis=0)  # expand dimension for batch size
+    return image_arr_normalized
+    
+def show_image(image_path) :
+    im =load_image()
+    
 #####
 def display_image(url, caption=None):
     st.markdown("<p></p>",unsafe_allow_html=True)
