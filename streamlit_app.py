76f377bc9360cbcc645f8fc8fdb41c6edd539202
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-08 16:33:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,7 +119,7 @@ pages = {
     "Gallery": gallery
 }
 # Create a sidebar with page selection
-page_selection = st.sidebar.radio("Go to", list(pages.keys()))
+page_selection = st.sidebar.radio("Make a choice", list(pages.keys()))
 # Run the selected page function
 pages[page_selection]()
 
