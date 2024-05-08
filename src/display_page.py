e7b297a604797ba120b6e076d8d26739cb3b332e
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-08 22:52:11 +0200

    Update display_page.py

--- src/display_page.py
+++ src/display_page.py
@@ -14,11 +14,7 @@ import tensorflow as tf
     
 
 #####
-def display_image(url, caption=None):
-    st.markdown("<p></p>",unsafe_allow_html=True)
-    image_url = url
-    st.image(image_url, caption=caption, use_column_width=True)
-    st.markdown("<p></p>",unsafe_allow_html=True)
+
 
 
 ##############
