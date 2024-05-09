42e94d9bd14dc607dd3767ab5385e243070901e2
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-09 14:30:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,23 +11,7 @@ import pages
 # from page_utils import font_modifier, display_image
 
 ########
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
 
-
-def make_font_poppins():
-    with open("src/styles.css") as css:
-        st.markdown(f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-
-    # Render the custom CSS style
-    st.markdown("<link href='https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap' rel='stylesheet'>", unsafe_allow_html=True)
-
-    # Hide Streamlit style
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 ########
 
