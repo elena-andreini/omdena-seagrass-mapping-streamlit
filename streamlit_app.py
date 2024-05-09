13d6432ce39f674d8fa87523e3be0ba3782407fc
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-09 14:17:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,12 +117,12 @@ def gallery():
 # Create a dictionary to map page names to their respective functions
 pages = {
     "Home": pages.homepage,
-    "About": about,
-    "Classify Image": classify,
-    "User Guide": guide,
-    "Datasets": datasets,
-    "Repository": repository,
-    "Gallery": gallery
+    "About": pages.about,
+    "Classify Image": pages.classify,
+    "User Guide": pages.guide,
+    "Datasets": pages.datasets,
+    "Repository": pages.repository,
+    "Gallery": pages.gallery
 }
 # Create a sidebar with page selection
 page_selection = st.sidebar.radio("Where do you want to go?", list(pages.keys()))
