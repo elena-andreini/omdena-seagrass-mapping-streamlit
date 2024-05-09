2e83aabeedd5bd4fa09487f6d26f33b6e2fefd88
Christopher Chitimbwa <chitimbwasc@yahoo.com>
2024-05-09 14:39:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src'))
-import streamlit as st
+# import streamlit as st
 # import utils_v2
 import pages
 
