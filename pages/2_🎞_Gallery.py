3889cd50568db6fbc636864850dff96206a242bf
NPriyankaDS <nprinka235@gmail.com>
2024-06-02 14:10:35 +0530

    changes in gallery.py

--- pages/2_🎞_Gallery.py
+++ pages/2_🎞_Gallery.py
@@ -1,13 +1,12 @@
 import streamlit as st
+import os
 
 st.title("Gallery :film_frames:")
 st.markdown("*This page displays the images, masks and their predictions for Greece and Croatia regions*")
 
+base_path = "model_utils"
 # Displaying the images
-st.image("model_utils\Greece_images\image1.JPG", caption='Greece image1')
-st.image("model_utils\Greece_images\image2.JPG", caption='Greece image2')
-st.image("model_utils\Croatia_images\image1.JPG", caption='Croatia image1')
-st.image("model_utils\Croatia_images\image2.JPG", caption='Croatia image2')
-
-
-
+st.image(os.path.join(base_path, "Greece_images", "image1.JPG"), caption='Greece image1')
+st.image(os.path.join(base_path, "Greece_images", "image2.JPG"), caption='Greece image2')
+st.image(os.path.join(base_path, "Croatia_images", "image1.JPG"), caption='Croatia image1')
+st.image(os.path.join(base_path, "Croatia_images", "image2.JPG"), caption='Croatia image2')
